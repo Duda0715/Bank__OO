99425c1dc80f6d4838176e364b8550fd72335664
Guilherme <silveiramaria.eduarda17@gmail.com>
2021-09-22 10:16:44 -0300

    Atualizei na aula de 22-09

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -1,33 +1,23 @@
 ﻿using System;
 
-namespace Bank
+namespace myBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaDuda = new ContaCorrente("Duda", 1234, 789, 200);
-            ContaCorrente contaDoGui = new ContaCorrente("Gui", 4321, 987, 100);
-            ContaCorrente contaDoLucas = new ContaCorrente("Lucas", 1111, 222, 333);
+            ContaCorrente contaDaMaria = new ContaCorrente("Maria", 1234, 100);
+            contaDaBarbara.Conta = 789;
 
-           Console.WriteLine("O titular da conta é:" + contaDaDuda.titular);
-           Console.WriteLine("O titular da conta é:" + contaDoGui.titular);
-           Console.WriteLine("O titular da conta é:" + contaDoLucas.titular);
+            ContaCorrente contaDoGui = new ContaCorrente("Gui", 1234, 120);
+            ContaCorrente contaDoLucas = new ContaCorrente("Lucas", 1234, 150);
 
-           bool retorno = contaDaDuda.Sacar(50);
-           Console.WriteLine(retorno);
-           Console.Write("O saldo da conta da Duda é:" + contaDaDuda.saldo);
-
-           contaDaDuda.Depositar(150);
-           Console.WriteLine("O saldo da conta da Duda é " + contaDaDuda.saldo);
-
-           contaDaDuda.Transferir(50, contaDoGui);
-           Console.WriteLine("O saldo da conta da Duda é " + contaDaDuda.saldo);
-           Console.WriteLine("O saldo da conta do Gui é " + contaDoGui.saldo);
-
-
-
-
+            contaDaBarbara.Sacar (100);
+           
+            Console.WriteLine("O número da conta da Maria é: " + contaDaMaria.Conta);
+            Console.WriteLine("O saldo da conta da Maria é: " + contaDaMaria.Saldo);
+            Console.WriteLine("O número da conta da Gui é: " + contaDoGui.Conta);
+            Console.WriteLine("O saldo da conta do Gui é: " + contaDoGui.Saldo);
         }
     }
 }
--- myBank/contaCorrente.cs
+++ myBank/contaCorrente.cs
@@ -1,37 +1,52 @@
 public class ContaCorrente{
-    public string titular;
-    public int agencia;
-    public int conta;
-    public double saldo;
+    
+    public string Titular { get; set; }
+    public int Agencia { get; set; }
+    public int Conta { get; set; }
+
+    private double _saldo; 
+
+    public double Saldo { 
+        get{
+            return this._saldo;
+        }
+        set{
+            if (value >= 0){
+                this._saldo = value;
+            }
+        } 
+    }
 
     public ContaCorrente() {}
 
-    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, int contacorrente_conta, double contacorrente_saldo) { 
-        titular = contacorrente_titular;
-        agencia = contacorrente_agencia; 
-        conta = contacorrente_conta; 
-        saldo = contacorrente_saldo; 
-        } 
+    public ContaCorrente(string contacorrente_titular, int contacorrente_agencia, double contacorrente_saldo)
+    { 
+        Titular = contacorrente_titular;
+        Agencia = contacorrente_agencia;
+        Saldo = contacorrente_saldo;
+    }
 
-        public bool Sacar (double valor){ 
-            if(this.saldo < valor){ 
-                return false; 
-                } 
-                else{ 
-                    this.saldo -= valor; 
-                    return true; 
-                    } 
-                    } 
-        public void Depositar (double valor){ 
-            this.saldo += valor; 
-            } 
-        public bool Transferir (double valor, ContaCorrente contaDestino){ 
-            if (this.saldo < valor){ return false; 
-            }
-             else{ 
-                 this.Sacar(valor); 
-                 contaDestino.Depositar(valor); 
-                 return true; 
-                 } 
-                } 
-            }
+    public bool Sacar (double valor){
+        if(this.Saldo < valor){
+            return false;
+        }
+        else{
+            this.Saldo -= valor;
+            return true;
+        }
+    }
+
+    public void Depositar (double valor){
+        this.Saldo += valor;
+    }
+
+    public bool Transferir (double valor, ContaCorrente contaDestino){
+        if (this.Saldo < valor){
+            return false;
+        }
+        else{
+            this.Sacar(valor);
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
